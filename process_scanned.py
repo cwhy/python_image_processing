6a4211246983d05cf1d2827aa43e96da9c3f9cd6
Chen Yu <chenyu.nus@gmail.com>
2017-05-19 10:05:41 +0000

    cleaned code

--- process_scanned.py
+++ process_scanned.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 import numpy as np
@@ -8,7 +7,7 @@ from scipy.misc import imsave
 o3 = ndimage.imread('Kids_DrawA_Otter3.jpg')
 
 
-o3_not0 = o3_sum(2) <= 254*3
+o3_not0 = o3.sum(2) <= 254*3
 
 if_crop_d1 = []
 for i, v in enumerate(o3_not0.sum(1)):
@@ -20,18 +19,19 @@ for i, v in enumerate(o3_not0.sum(1)):
         if_crop_d1.append(False)
 
 
-
 o3_cropped = o3[np.invert(np.array(if_crop_d1)), :, :]
 
 m = np.min(o3_cropped, 2)
 alpha = 1 - m/255
 
-def div0( a, b ):
+
+def div0(a, b):
     """ ignore / 0, div0( [-1, 0, 1], 0 ) -> [0, 0, 0] """
     with np.errstate(divide='ignore', invalid='ignore'):
-        c = np.floor_divide( a, b )
-        c[ ~ np.isfinite( c )] = 0  # -inf inf NaN
+        c = np.floor_divide(a, b)
+        c[~ np.isfinite(c)] = 0  # -inf inf NaN
     return c
+
 
 new_rgb = div0((o3_cropped - m[:,:,np.newaxis]), alpha[:,:,np.newaxis])
 
